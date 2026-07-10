f427554df9610fd8512fad4e726c566a5989da25
jtl-praktikant-nico <jtlpraktikant3@outlook.de>
2026-07-10 10:38:12 +0200

    ordner für die pages erstellt

---
+++ Frontend/src/pages/dashboard.jsx
@@ -0,0 +1,13 @@
+import {
+  AppHeader,
+  Badge,
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  Separator,
+  Text,
+} from "@jtl-software/platform-ui-react";
+import { Bell, LayoutDashboard, Package, Settings, ShoppingCart } from "lucide-react";
