d0f7406b61e4510e33cabb8b7f0bc79b5975e54f
jtl-praktikant-nico <jtlpraktikant3@outlook.de>
2026-07-15 14:53:54 +0200

    c# klasse ActionRecommendation.cs erstellt und defieniert

--- RevolvAPI/Data/AppDbContext.cs
+++ RevolvAPI/Data/AppDbContext.cs
@@ -6,10 +6,9 @@ namespace RevolvAPI.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<ActionRecommendation> ActionRecommendations { get; set; }
     }
 }
---
+++ RevolvAPI/Models/ActionRecommendation.cs
@@ -0,0 +1,6 @@
+﻿namespace RevolvAPI.Models
+{
+    public class ActionRecommendation
+    {
+    }
+}
