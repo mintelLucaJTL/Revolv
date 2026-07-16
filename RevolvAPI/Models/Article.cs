2aa65ed6688d112852ea890449c4f3fe4741b1c3
jtlLbPRK2 <jtlpraktikant2@outlook.de>
2026-07-16 09:21:26 +0200

    Article.cs & QualityIssue.cs
    Datenbank Tabellen angelegt

--- RevolvAPI/Models/Article.cs
+++ RevolvAPI/Models/Article.cs
@@ -5,5 +5,11 @@ namespace RevolvAPI.Models
     [Table("Articles", Schema = "revolv")]
     public class Article
     {
+        public int Id { get; set; }
+        public string? ArticleNumber { get; set; }
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public String? Size { get; set; }
+
     }
 }
--- RevolvAPI/Models/QualityIssue.cs
+++ RevolvAPI/Models/QualityIssue.cs
@@ -5,5 +5,12 @@ namespace RevolvAPI.Models
     [Table("QualityIssues", Schema = "revolv")]
     public class QualityIssue
     {
+        public int Id { get; set; }
+        public int AiRecommendationId { get; set; }
+        public string? IssueText { get; set; }
+        public string? Status { get; set; } = "Ausstehend";
+
+        // Navigation property
+        public AiRecommendation AiRecommendation { get; set; } = null!;
     }
 }
