1496af91ccaff70e22b454a47ba75cf393232752
mintelLucaJTL <JTLPraktikant4@outlook.de>
2026-07-16 09:34:08 +0200

    added better security to our login and register

--- RevolvAPI/DTOs/RegisterRequest.cs
+++ RevolvAPI/DTOs/RegisterRequest.cs
@@ -1,8 +1,14 @@
-﻿namespace RevolvAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RevolvAPI.DTOs
 {
     public class RegisterRequest
     {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
         public string Password { get; set; } = string.Empty;
     }
 }
